2f877bb372fe38c720affb7ba103d56a9b8f97f9
Hoxtygen <hoxtygen@live.com>
2019-07-22 13:31:27 +0100

    Fix server error

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const server = express();
-server.use(express.json);
+
+server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
-server.get('/', (req, res) => res.status(200).json({
-  message: 'Welcome to the introductory class on web authentication.',
-}));
+server.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to the introductory class on web authentication.',
+  });
+});
 
 module.exports = server;
